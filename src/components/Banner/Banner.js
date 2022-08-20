a5729ff80f6a2274b03299a72d0c3feac3bb91ee
Sparsh <res.sparsh@gmail.com>
2022-08-20 22:10:39 +0530

    A new Crypto Portfolio website

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Portfolio
+            Crypto Detective
           </Typography>
           <Typography
             variant="subtitle2"
--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Portfolio
+              Crypto Detective
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
